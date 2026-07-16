7464787c6ab26e455742016edf6aa1918c4338dd
Ehsan Zehtabchi <zehtabchi.info@gmail.com>
2026-07-16 21:24:37 +0200

    Adding GPIO init function by LL.

---
+++ STM32_GPIO_LL/Core/Inc/GPIO_LL.h
@@ -0,0 +1,13 @@
+/*
+ * GPIO_LL.h
+ *
+ *  Created on: 16 Jul 2026
+ *      Author: Zehta
+ */
+
+#ifndef INC_GPIO_LL_H_
+#define INC_GPIO_LL_H_
+
+void LED_init(void);
+
+#endif /* INC_GPIO_LL_H_ */
